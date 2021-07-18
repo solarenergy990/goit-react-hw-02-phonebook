aa24349773d7ef89c2406c165ae406243f7490d3
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-07-19 00:34:45 +0200

    Updates

--- static/js/main.6e85d9a1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n.n(o),r=n(3),s=n.n(r),i=(n(8),n(9),n(1));var a=function(){return Object(i.jsx)("div",{className:"App"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.6e85d9a1.chunk.js.map
